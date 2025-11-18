d9c44c4bf50cc42da16760a9de48d9aa1df2afb4
Naman Pandey <nkpandey.bsr@gmail.com>
2025-11-18 13:06:57 +0530

    Footer Created

---
+++ portfolio-demo/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer className="py-10">
+      <div className="mx-auto px-3">
+
+        <div className="text-center">
+          <p className="">
+            © <span>Copyright</span>
+            <strong className="px-1 font-bold">MyPortfolio</strong>
+            <span>All Rights Reserved</span>
+          </p>
+        </div>
+
+        <div className="flex justify-center mt-5">
+              <a href=""></a>
+              <a href=""></a>
+              <a href=""></a>
+              <a href=""></a>
+        </div>
+      </div>
+    </footer>
+  );
+}
