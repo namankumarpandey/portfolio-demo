55318a0a489c42ae327b2102105bebb3ddce4a41
Naman Pandey <nkpandey.bsr@gmail.com>
2025-11-18 13:07:43 +0530

    Footer Created with responsiveness

--- portfolio-demo/src/components/Footer.jsx
+++ portfolio-demo/src/components/Footer.jsx
@@ -1,8 +1,11 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { FaLinkedin } from "react-icons/fa6";
 export default function Footer() {
   return (
-    <footer className="py-10">
-      <div className="mx-auto px-3">
-
+    <footer className="py-10 text-sm bg-[#f9f9f9] text-[#212529] relative">
+      <div className="portfolio-footer w-full px-3 mx-auto">
         <div className="text-center">
           <p className="">
             © <span>Copyright</span>
@@ -12,10 +15,33 @@ export default function Footer() {
         </div>
 
         <div className="flex justify-center mt-5">
-              <a href=""></a>
-              <a href=""></a>
-              <a href=""></a>
-              <a href=""></a>
+          <a
+            href=""
+            className="flex items-center justify-center w-10 h-10 border border-[color-mix(in_srgb,var(--default-color),transparent_50%) rounded-full mx-1.25 hover:text-(--accent-color) hover:border-(--accent-color)"
+          >
+            <FaXTwitter />
+          </a>
+          <a
+            href=""
+            className="flex items-center justify-center w-10 h-10 border border-[color-mix(in_srgb,var(--default-color),transparent_50%) rounded-full mx-1.25 hover:text-(--accent-color) hover:border-(--accent-color)"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href=""
+            className="flex items-center justify-center w-10 h-10 border border-[color-mix(in_srgb,var(--default-color),transparent_50%) rounded-full mx-1.25 hover:text-(--accent-color) hover:border-(--accent-color)"
+          >
+            <FiInstagram />
+          </a>
+          <a
+            href=""
+            className="flex items-center justify-center w-10 h-10 border border-[color-mix(in_srgb,var(--default-color),transparent_50%) rounded-full mx-1.25 hover:text-(--accent-color) hover:border-(--accent-color)"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+        <div className="text-center mt-2.5 text-[13px]">
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
       </div>
     </footer>
